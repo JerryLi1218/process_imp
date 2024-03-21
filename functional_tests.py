3ac8c23cf4e4c4cafdca5d0c5b8aa0d1de942502
Jerry_li_1218 <2406700175@qq.com>
2024-03-21 13:48:38 +0800

    Feature: [msg2]The first functional test.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class New_visitor_test(unittest.TestCase):
 
-assert 'Django' in browser.page_source
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''
+        some one hear that there is an online application for To-Do list
+        he is going to take a look 
+        '''
+        self.browser.get('http://localhost:8000')
+
+        # He notes that there is a "To-Do" in the home page
+        self.assertIn('To-Do', self.browser.title), "browser title was:" + self.browser.title
+        self.fail('Finish the test')
+
+        # The application has a text entry field for to-do item
+
+        # He typed "Buy Flower" in the text box (so romantic~)
+        
+        # He visited the URL and found that his list was still there
+        
+        # He left satisfied
+
+if __name__ == '__main__':
+    unittest.main()
